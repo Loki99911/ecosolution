0c65fc5693428bef36c987b6d71e72a6831b4022
Loki99911 <nikita_kresik@i.ua>
2023-12-04 16:07:36 +0200

    add img

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -5,22 +5,26 @@ import {
   MainText,
   MainTitle,
 } from './MainSection.styled';
+import mainImg from '../../../public/main.png';
 
 export default function MainSection() {
   return (
-    <MainSectionWrapper>
-      <MainTitle>RENEWABLE ENERGY For any task</MainTitle>
-      <div>
-        <MainText>
-          Development and implementation of renewable non-polluting energy
-          sources, generating power generation using energy wind, sun, water,
-          biomass
-        </MainText>
-        <LinkToLearnMore to={'#contactUs'}>
-          Learn more
-          <SVGFromSprite name="arrow-right" width="32px" height="32px" />
-        </LinkToLearnMore>
-      </div>
-    </MainSectionWrapper>
+    <>
+      <MainSectionWrapper>
+        <MainTitle>RENEWABLE ENERGY For any task</MainTitle>
+        <div>
+          <MainText>
+            Development and implementation of renewable non-polluting energy
+            sources, generating power generation using energy wind, sun, water,
+            biomass
+          </MainText>
+          <LinkToLearnMore to={'#contactUs'}>
+            Learn more
+            <SVGFromSprite name="arrow-right" width="32px" height="32px" />
+          </LinkToLearnMore>
+        </div>
+      </MainSectionWrapper>
+      <img src={mainImg} alt="Wind turbine" />
+    </>
   );
 }
